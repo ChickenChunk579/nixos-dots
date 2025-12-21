b4a3c990ae8ffe4619dacb2ae540fec773207d4e
Rhys Tumelty <rhys@tumelty.co.uk>
2025-12-21 15:43:22 +0000

    Changed default.nix of gamescope-session-steam

--- nixpkgs/gamescope-session-steam/default.nix
+++ nixpkgs/gamescope-session-steam/default.nix
@@ -1,25 +1,25 @@
-{ pkgs ? import (fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/4fe8d07066f6ea82cda2b0c9ae7aee59b2d241b3.tar.gz";
-    sha256 = "sha256:06jzngg5jm1f81sc4xfskvvgjy5bblz51xpl788mnps1wrkykfhp";
-  }) {} }:
+# /etc/nixos/nixpkgs/gamescope-session-steam/default.nix
+{ pkgs ? import <nixpkgs> {}, lib ? pkgs.lib, stdenv ? pkgs.stdenv, fetchgit ? pkgs.fetchgit, ... }:
 
-pkgs.stdenv.mkDerivation rec {
+stdenv.mkDerivation rec {
   pname = "gamescope-session-steam";
   version = "0.1.0";
 
-  src = pkgs.fetchgit {
+  src = fetchgit {
     url = "https://github.com/ChimeraOS/gamescope-session-steam";
     rev = "1a3fdb7fa15a4bba7204bef69702b7a10a297828";
     sha256 = "sha256-jlrqb4GReTETCke5klrkRFIafKy/k/icNi7CmgziuLk=";
   };
 
-  buildInputs = [];
-
-  buildPhase = "true";  # Do nothing in build phase
-  configurePhase = "true";  # Do nothing in configure phase
+  # ChimeraOS scripts often need specific paths adjusted for NixOS
   installPhase = ''
     mkdir -p $out
-    cp -r $src/usr $out/usr  # Copy the 'usr' directory from the source to the output
+    cp -r usr/* $out/
   '';
+
+  meta = with lib; {
+    description = "Gamescope session scripts for Steam";
+    license = licenses.mit; # Verify license from source
+  };
 }
 
