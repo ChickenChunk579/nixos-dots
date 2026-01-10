f36370438790af3557adb32747374eab53dcc953
Rhys Tumelty <rhys@tumelty.co.uk>
2026-01-10 21:42:22 +0000

    polish changes

--- modules/home/hyprland/startup.nix
+++ modules/home/hyprland/startup.nix
@@ -9,7 +9,7 @@
     "hyprctl setcursor material_light_cursors 32"
     "dbus-update-activation-environment WAYLAND_DISPLAY DISPLAY XDG_CURRENT_DESKTOP=Hyprland SWAYSOCK I3SOCK XCURSOR_SIZE XCURSOR_THEME"
     "swaync"
-    "swayosd"
+    "swayosd-server"
     "steam -silent"
   ];
 }
