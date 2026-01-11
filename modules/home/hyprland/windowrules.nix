d6f6c7582624b74a1a1ad7c1f2f64cb98c9ddc84
Rhys Tumelty <rhys@tumelty.co.uk>
2026-01-11 10:28:01 +0000

    window rules for on screen keyboard

--- modules/home/hyprland/windowrules.nix
+++ modules/home/hyprland/windowrules.nix
@@ -6,5 +6,8 @@
   ];
   wayland.windowManager.hyprland.settings.windowrulev2 = [
     "float,title:(swayosd-server)"
+    "float, initialTitle:^(Steam Input On-screen Keyboard)$"
+    "stayfocused, initialTitle:^(Steam Input On-screen Keyboard)$"
+    "pin, initialTitle:^(Steam Input On-screen Keyboard)$"
   ];
 }
