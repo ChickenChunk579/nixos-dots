9006b604d738fc8fed1a4bedc132942410248640
Rhys Tumelty <rhys@tumelty.co.uk>
2026-01-25 08:11:48 +0000

    Some random config changes? idk but may have fixed some steamdeck builds

--- menu.sh
+++ menu.sh
@@ -1,10 +1,10 @@
 #!/run/current-system/sw/bin/bash
-ROOT_RESULT=$(echo -e "Terminal\nLauncher\nWorkspace Left\nWorkspace Right\nScreenshot (Copy)\nChange Wallpaper\nChange Shaders\nKill Active\nLock\nFloat\nPower" | walker --dmenu)
+ROOT_RESULT=$(echo -e "Terminal\nLauncher\nWorkspace Left\nWorkspace Right\nScreenshot (Copy)\nChange Wallpaper\nChange Shaders\nKill Active\nLock\nFloat\nPower" | wofi --show dmenu --cache-file /dev/null)
 if [[ "$ROOT_RESULT" == "Terminal" ]]; then
   hyprctl dispatch exec "kitty"
 fi
 if [[ "$ROOT_RESULT" == "Launcher" ]]; then
-  hyprctl dispatch exec "walker"
+  hyprctl dispatch exec "wofi --show drun"
 fi
 if [[ "$ROOT_RESULT" == "Workspace Left" ]]; then
   hyprctl dispatch workspace r-1
@@ -31,7 +31,7 @@ if [[ "$ROOT_RESULT" == "Kill Active" ]]; then
   hyprctl dispatch killactive
 fi
 if [[ "$ROOT_RESULT" == "Power" ]]; then
-  POWER_RESULT=$(echo -e "Shutdown\nReboot\nReturn to Steam" | walker --dmenu)
+  POWER_RESULT=$(echo -e "Shutdown\nReboot\nReturn to Steam" | wofi --show dmenu --cache-file /dev/null)
 
   if [[ "$POWER_RESULT" == "Shutdown" ]]; then
     shutdown now
